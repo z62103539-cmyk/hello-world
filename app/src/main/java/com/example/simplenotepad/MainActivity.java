58d0ce3047eafc16dd2f57e0316e28ed37937622
Yu_Zheng <z62103539@gmail.com>
2026-03-29 22:53:14 -0400

    Week 3: Finalized Activity navigation and UI layout structure.

--- app/src/main/java/com/example/simplenotepad/MainActivity.java
+++ app/src/main/java/com/example/simplenotepad/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.simplenotepad;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +16,29 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        View mainView = findViewById(R.id.main);
+        if (mainView != null) {
+            ViewCompat.setOnApplyWindowInsetsListener(mainView, (v, insets) -> {
+                Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+                return insets;
+            });
+        }
+
+        Button btnAddNote = findViewById(R.id.btn_add_note);
+        if (btnAddNote != null) {
+            btnAddNote.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(MainActivity.this, EditNoteActivity.class);
+                    startActivity(intent);
+                }
+            });
+        }
     }
 }
